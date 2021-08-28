0d1c6b5e56fbf384bfb40be7890f8be2ccafd5ae
Terima <tralop15@gmail.com>
2021-08-28 09:36:12 +0800

    added 'News' model

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -1,13 +1,13 @@
 <?php
 
 namespace App\Http\Controllers;
-use Illuminate\Support\Facades\DB;
+use App\Models\News;
 
 class NewsController extends Controller
 {
     public function index()
     {
-        $news = DB::table('news')->get();
+        $news = News::all();
         return $news;
     }
 }
