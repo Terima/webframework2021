ba1d17ba2876b53c942eb41076584628deddf9c3
Terima <tralop15@gmail.com>
2021-08-28 09:22:17 +0800

    created NewsController and tried the routing

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/news', function () {
+    $news = DB::table('news')->get();
+    return $news;
 });
